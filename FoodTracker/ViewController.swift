1eaacb04b465755e4652220677ba70d655699829
tayutaedomo <tayutaedomo@gmail.com>
2019-08-19 16:40:41 +0900

    Add project files

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by tayutaedomo on 2019/08/19.
+//  Copyright © 2019 tayutaedomo.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
